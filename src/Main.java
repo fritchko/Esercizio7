8601c2ddf62ac1062a1322b7b8ceb04c3dc7e6bd
Davide Francesco Riccio <ledrazors@gmail.com>
2023-06-09 10:44:20 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        String myname = "Davide";
+
+        if(myname.length() % 2 == 0){
+            System.out.println("Le lettere del mio nome sono un numero pari.");
+        } else {
+            System.out.println("Le lettere del mio nome sono un numero dispari.");
+        }
+    }
+}
